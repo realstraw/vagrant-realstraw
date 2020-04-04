589f03d40790b8892e75381693fcdf3d1229bab8
Kexin Xie <kexin.xie@salesforce.com>
2020-04-04 15:33:53 -0700

    add ssh forward and ship configuration script for tmux

--- Vagrantfile
+++ Vagrantfile
@@ -60,8 +60,12 @@ Vagrant.configure("2") do |config|
   # View the documentation for the provider you are using for more
   # information on available options.
 
+  # Forward ssh agent
+  config.ssh.forward_agent = true
+
   # Enable provisioning with a shell script. Additional provisioners such as
   # Ansible, Chef, Docker, Puppet and Salt are also available. Please see the
   # documentation for more information about their specific syntax and use.
   config.vm.provision "shell", path: "bootstrap.sh"
+
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,28 @@
+echo "Update everthing to the latest..."
 pacman -Syu --noconfirm
 
-pacman -S --noconfirm base-devel aws-cli terraform neovim
+echo "Install the necessary packages..."
+pacman -S --noconfirm \
+    base-devel \
+    aws-cli \
+    terraform \
+    neovim \
+    git \
+    zsh \
+    tmux
+
+echo "Generating the script to configure vagrant user..."
+
+cat > configure_me << SCRIPT
+#!/usr/bin/env bash
+echo "Apply upstream configurations..."
+rm -rf upstream-src
+mkdir -p upstream-src
+
+echo "- Configuring tmux..."
+git clone git@github.com:realstraw/dot-tmux.git upstream-src/dot-tmux
+upstream-src/dot-tmux/bootstrap.sh
+SCRIPT
+
+chown vagrant:vagrant configure_me
+chmod +x configure_me
